ac221c16885fafe02e05bb6ee2b1f39b1c154308
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-28 21:05:52 +0100

    Adding forseti and complete demo

---
+++ Source/Toodeloo.Web.vNext/demo/index.js
@@ -0,0 +1,32 @@
+(function() {
+	function Employee(firstName, lastName) {
+		var self = this;
+		
+		this.firstName = ko.observable(firstName);
+		this.lastName = ko.observable(lastName);
+		
+		this.fullName = ko.computed(function() {
+			return this.firstName() + " " + this.lastName();
+		},this);
+		
+		this.clone = function() {
+			return new Employee(self.firstName(), self.lastName());
+		}
+	}
+	
+	
+	function ViewModel() {
+		var self = this;
+		
+		this.employees = ko.observableArray();
+		this.currentEmployee = new Employee("","");
+		
+		this.add = function() {
+			self.employees.push(self.currentEmployee.clone());
+		}
+	}
+	
+	$(function() {
+		ko.applyBindings(new ViewModel());
+	})
+})();
