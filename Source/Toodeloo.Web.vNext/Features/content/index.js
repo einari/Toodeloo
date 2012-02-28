35d3c8416345740cc15309f56bc06a5f883f623d
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-28 19:08:43 +0100

    Brought in the latest version of Bifrost and altered things to use the messenger and the validation!

--- Source/Toodeloo.Web.vNext/Features/content/index.js
+++ Source/Toodeloo.Web.vNext/Features/content/index.js
@@ -1,1 +1,34 @@
-﻿
+﻿(function() {
+	function itemAdded() {}
+	function itemDeleted() {}
+	
+    Bifrost.features.featureManager.get("content/index").defineViewModel(function () {
+		function Item(title) {
+			var self = this;
+			this.title = ko.observable(title).extend({validation: {required: {message:"Title is required"}}});
+			this.title.validator.validate(title);
+			
+			this.clone = function() {
+				return new Item(self.title());
+			}
+		}
+	
+		var self = this;
+		this.itemToCreate = new Item("");
+				
+		this.items = ko.observableArray();
+		
+		this.addItem = function() {			
+			var item = self.itemToCreate.clone();
+			self.items.push(item);
+			self.itemToCreate.title("");
+			
+			Bifrost.messaging.messenger.publish(new itemAdded());
+		}
+		
+		this.deleteItem = function(item) {
+			self.items.remove(item);
+			Bifrost.messaging.messenger.publish(new itemDeleted());
+		}
+	});
+})();
---
+++ Source/Toodeloo.Web.vNext/Features/layout/header.js
@@ -0,0 +1,14 @@
+(function() {
+    Bifrost.features.featureManager.get("layout/header").defineViewModel(function () {
+		var self = this;
+		
+		this.itemCount = ko.observable(0);
+		
+		Bifrost.messaging.messenger.subscribeTo("itemAdded", function() {
+			self.itemCount(self.itemCount()+1);
+		});
+		Bifrost.messaging.messenger.subscribeTo("itemDeleted", function() {
+			self.itemCount(self.itemCount()-1);
+		});
+	});
+})();
