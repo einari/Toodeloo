b32d256742e3d1751029c5cc1bb287e19ed34767
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-28 20:12:16 +0100

    Finetuning MVC / jQuery story

---
+++ Source/Toodeloo.Web/Content/scripts/index.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    var todoInput = $(".labelInput input"),
+        todoList = $("ul.todo-list"),
+        saveButton = $(".sidebar li.save");
+
+
+    todoInput.keypress(function (e) {
+
+        if (e.which != 13) return;
+        addTaskItem();
+    });
+
+    saveButton.click(function () {
+        if (todoInput.val().length)
+            addTaskItem();
+    });
+
+    todoList.delegate("a.delete-task", "click", function () {
+        var task = $(this).parent("li");
+        task.fadeOut("fast").remove();
+    });
+
+    function addTaskItem() {
+        var listItem = createListItem(todoInput.val());
+        todoList.append(listItem.fadeIn());
+        todoInput.val("");
+    }
+
+
+    function createListItem(task) {
+        var deleteTask = $("<a/>").addClass("delete-task")
+                                  .append(
+                                    $("<img/>").attr("src", "/Content/images/delete.png")
+                                  );
+        return $("<li/>").text(task).append(deleteTask);
+    }
+
+
+});
+
