99e87748eea75930d889ed883b45f208f7037a30
Einar Ingebrigtsen <einar@dolittle.com>
2012-08-15 22:42:28 +0200

    Yet another spec

---
+++ Source/Toodeloo.WinRT.Specs/Features/content/for_index/when_item_count_is_published.js
@@ -0,0 +1,15 @@
+﻿describe("when item count is published", function () {
+    var viewModel;
+
+    beforeEach(function () {
+        viewModel = new Toodeloo.Features.content.Index({
+            getAll: function () { return ko.observableArray(); }
+        }).viewModel();
+
+        $.publish("itemCount", [5]);
+    });
+
+    it("should add an item to items", function () {
+        expect(viewModel.count()).toBe(5);
+    });
+});
